8273fe0b17927b03e3d0fced68add1b2f46fb398
Siajiza <>
2021-04-07 19:25:49 +0000

    Add a float 0.2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 meal_completed = True
 sub_total = 100
-tip = sub_total * 1/5
+tip = sub_total * 0.2
 total = sub_total + tip
 receipt = "Thank for your visit, Your total is " + str(total) + " €" 
 
