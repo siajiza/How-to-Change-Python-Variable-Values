25d55e8d61534cbac5878a55a32432712ca091b5
Siajiza <>
2021-04-07 18:52:58 +0000

    Add changes to the sentence of the receipt

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ meal_completed = True
 sub_total = 100
 tip = sub_total * 1/5
 total = sub_total + tip
-receipt = "Your total is " + str(total) + " €" 
+receipt = "Thank for your visit, Your total is " + str(total) + " €" 
 
 print(receipt)
